af741ce873cd6f59dedec3841adecaebdf113e39
Evan Doyle <evanmdoyle@gmail.com>
2024-04-17 14:04:04 -0700

    Extract serialization for stacktrace, var dicts, use simple str cast impl for now

--- chatpdb/parsing/interface.py
+++ chatpdb/parsing/interface.py
@@ -2,6 +2,8 @@ from types import FrameType
 
 from chatpdb.frame import FrameData
 from chatpdb.chat import AskArgs, ExplainArgs
+from chatpdb.parsing.stacktrace import serialize_stacktrace
+from chatpdb.parsing.vars import serialize_vars
 
 
 def parse_ask_args_from_frame(frame: FrameType, message: str) -> AskArgs:
@@ -9,11 +11,9 @@ def parse_ask_args_from_frame(frame: FrameType, message: str) -> AskArgs:
     return AskArgs(
         message=message,
         source_code=frame_data.source_code,
-        stack_trace=list(map(str, frame_data.stack_trace)),
-        local_vars={},
-        global_vars={}
-        # local_vars=json.loads(json.dumps(frame_data.locals_dict)),
-        # global_vars=json.loads(json.dumps(frame_data.globals_dict)),
+        stack_trace=serialize_stacktrace(frame_data.stack_trace),
+        local_vars=serialize_vars(frame_data.locals_dict),
+        global_vars=serialize_vars(frame_data.globals_dict),
     )
 
 
@@ -21,9 +21,7 @@ def parse_explain_args_from_frame(frame: FrameType) -> ExplainArgs:
     frame_data = FrameData.from_frame(frame=frame)
     return ExplainArgs(
         source_code=frame_data.source_code,
-        stack_trace=list(map(str, frame_data.stack_trace)),
-        local_vars={},
-        global_vars={}
-        # local_vars=json.loads(json.dumps(frame_data.locals_dict)),
-        # global_vars=json.loads(json.dumps(frame_data.globals_dict)),
+        stack_trace=serialize_stacktrace(frame_data.stack_trace),
+        local_vars=serialize_vars(frame_data.locals_dict),
+        global_vars=serialize_vars(frame_data.globals_dict),
     )
